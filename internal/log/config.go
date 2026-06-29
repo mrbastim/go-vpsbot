f240434962e5811bed6fc1dbb418841cbfa8ee25
Timur <78547482+mrbastim@users.noreply.github.com>
2026-06-29 14:55:55 +0000

    feat: logger implementation

--- internal/log/config.go
+++ internal/log/config.go
@@ -4,11 +4,43 @@ import (
 	"go.uber.org/zap"
 )
 
-var logger *zap.Logger
+var zapLogger *zap.Logger
 
-type Config struct {
-	Level      string
-	File       string
-	MaxSizeMB  int
-	MaxBackups int
+func InitLogger() {
+	var err error
+	config := zap.NewProductionConfig()
+	enccoderConfig := zap.NewProductionEncoderConfig()
+	config.EncoderConfig = enccoderConfig
+
+	zapLogger, err = config.Build(zap.AddCallerSkip(1))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Info(msg string, fields ...zap.Field) {
+	zapLogger.Info(msg, fields...)
+}
+
+func Warn(msg string, fields ...zap.Field) {
+	zapLogger.Warn(msg, fields...)
+}
+
+func Error(msg string, fields ...zap.Field) {
+	zapLogger.Error(msg, fields...)
+}
+
+func Fatal(msg string, fields ...zap.Field) {
+	zapLogger.Fatal(msg, fields...)
+}
+
+func Sync() error {
+	return zapLogger.Sync()
+}
+
+func GetLogger() *zap.Logger {
+	if zapLogger == nil {
+		panic("logger not initialized")
+	}
+	return zapLogger
 }
